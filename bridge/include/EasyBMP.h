e7d000e5cd9ea529c6a14730e6d3654f354e7bd0
Nastya <nastya.popper@gmail.com>
2017-11-23 21:22:09 +0300

    Update EasyBMP.h

--- bridge/include/EasyBMP.h
+++ bridge/include/EasyBMP.h
@@ -1,23 +1,3 @@
-/*************************************************
-*                                                *
-*  EasyBMP Cross-Platform Windows Bitmap Library * 
-*                                                *
-*  Author: Paul Macklin                          *
-*   email: macklin01@users.sourceforge.net       *
-* support: http://easybmp.sourceforge.net        *
-*                                                *
-*          file: EasyBMP.h                       * 
-*    date added: 01-31-2005                      *
-* date modified: 12-01-2006                      *
-*       version: 1.06                            *
-*                                                *
-*   License: BSD (revised/modified)              *
-* Copyright: 2005-6 by the EasyBMP Project       * 
-*                                                *
-* description: Main include file                 *
-*                                                *
-*************************************************/
-
 #ifdef _MSC_VER 
 // MS Visual Studio gives warnings when using 
 // fopen. But fopen_s is not going to work well 
