77f63fe8f04e3957d10bc79a00b4bc2cad14c102
Nastya <nastya.popper@gmail.com>
2017-11-23 21:19:50 +0300

    Update EasyBMP_BMP.h

--- externals/EasyBMP/include/EasyBMP_BMP.h
+++ externals/EasyBMP/include/EasyBMP_BMP.h
@@ -1,22 +1,4 @@
-/*************************************************
-*                                                *
-*  EasyBMP Cross-Platform Windows Bitmap Library *
-*                                                *
-*  Author: Paul Macklin                          *
-*   email: macklin01@users.sourceforge.net       *
-* support: http://easybmp.sourceforge.net        *
-*                                                *
-*          file: EasyBMP_VariousBMPutilities.h   *
-*    date added: 05-02-2005                      *
-* date modified: 12-01-2006                      *
-*       version: 1.06                            *
-*                                                *
-*   License: BSD (revised/modified)              *
-* Copyright: 2005-6 by the EasyBMP Project       *
-*                                                *
-* description: Defines BMP class                 *
-*                                                *
-*************************************************/
+
 
 #ifndef _EasyBMP_BMP_h_
 #define _EasyBMP_BMP_h_
